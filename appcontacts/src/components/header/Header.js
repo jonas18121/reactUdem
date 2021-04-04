feca3233be8e603b160dfc3a7d8788c7cc3ea6ba
jonas18121 <jonasxtrm971@gmail.com>
2021-04-04 13:29:26 +0200

    création du header

---
+++ appcontacts/src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-primary mb-3 py-0">
+                <div className='container'>
+                    <a href='/' className='navbar-brand'>AppContacts</a>
+
+                    <ul className='navbar-nav'>
+                        <li className='nav-item ml-auto'>
+                            <a href='/' className='nav-link'>Acceuil</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
