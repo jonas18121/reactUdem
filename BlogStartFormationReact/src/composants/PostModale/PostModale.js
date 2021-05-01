1187005d8efd5b0280c93255ac37cfd8499f9fd6
Jonas18121 <jonasxtrm971@gmail.com>
2021-05-01 12:22:54 +0200

    blog

---
+++ BlogStartFormationReact/src/composants/PostModale/PostModale.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import './PostModale.css'
+
+class PostModale extends Component {
+
+    render () {
+
+        return (
+                <div className="PostComplet">
+                    
+                    <h1>Titre</h1>
+                    <p>Contenu</p>
+            
+                    <button className="btn btn-danger my-3 btnPost">Fermer</button>
+                
+                </div>
+            
+            )
+        
+    
+    }
+}
+
+export default PostModale;
