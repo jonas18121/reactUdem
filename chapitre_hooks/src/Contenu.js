1d336ef3b62cb11c16ef52c83d8574bf36faf0a5
Jonas18121 <jonasxtrm971@gmail.com>
2021-05-17 21:58:30 +0200

    useEffect

--- chapitre_hooks/src/Contenu.js
+++ chapitre_hooks/src/Contenu.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react'
+import React, { useState, useEffect }  from 'react'
 
 export default function Contenu() {
 
@@ -12,6 +12,20 @@ export default function Contenu() {
         if(monState > 0 ) return setMonState(monState - 1 );
     }
 
+    useEffect(() => {
+        console.log('composant affiché, sans 2èmes paramètres');
+        console.log('Le piège ces que si on met setMonState(monState + 1 ) dans cette fonction, ça va faire une boucle infini');
+    });
+
+    useEffect(() => {
+        console.log('appeller useEffect une seule fois au 1er affichage, c\'est tout');
+        console.log('Avec un tableau vide en 2èmes paramètres, c\'est comme un componentDidMount()');
+    }, []);
+
+    useEffect(() => {
+        console.log('appeller useEffect au début et une seulement lorsque le state va être modifier, pas de boucle infini');
+        console.log('Avec une le state dans le tableau en 2èmes paramètres, c\'est comme un componentDidUpdate()');
+    }, [monState]);
 
     return (
         <div>
