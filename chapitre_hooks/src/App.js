4fed7746bc91597c9b8f05ff46d2317c443d1492
Jonas18121 <jonasxtrm971@gmail.com>
2021-05-23 17:01:44 +0200

    Créer une fenêtre modale

--- chapitre_hooks/src/App.js
+++ chapitre_hooks/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Contenu from './Components/Contenu'
+import Modale from './Components/Modale';
 
 function App() {
 
+    const [toggleModale, setToggleModale] = useState(false);
+
+    const closeModale = () => setToggleModale(false);
+
+    const openModale = () => setToggleModale(true);
+
+    const stopPropa = (event) => event.stopPropagation();  
+
+    
+    let myModale = '';
+
+    if (toggleModale) {
+        myModale = <Modale closeFunc={closeModale } stopPropa={stopPropa} />;    
+    }
+    else {
+        myModale = null;
+    }
+    
+
     return (
         <div className="App">
-            <Contenu />
+
+            <button onClick={openModale} >Ouvrir la modale</button>
+
+            {myModale}
         </div>
     );
 }
