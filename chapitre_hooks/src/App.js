77d64f461ce4ceac97cda29ec6ba0770bcb7b407
Jonas18121 <jonasxtrm971@gmail.com>
2021-06-02 23:12:40 +0200

    creer des onglets

--- chapitre_hooks/src/App.js
+++ chapitre_hooks/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar';
-import Compteur from './Components/Compteur';
+// import Navbar from './Components/Navbar';
+// import Compteur from './Components/Compteur';
+import Onglet from './Components/onglet/Onglet'
 
 
 function App() {
 
     return (
         <div className="App">
-            <Navbar />
-
-            <Compteur />
+            <Onglet />
         </div>
     );
 }
